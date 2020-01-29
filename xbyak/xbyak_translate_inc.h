cfe6fccb4ea38a74211f7e3212a05d30e14d1f31
Kentaro KAWAKAMI <kawakami.k@fujitsu.com>
2020-01-29 18:47:28 +0900

    xbyak_translate:add translation frame work

---
+++ xbyak/xbyak_translate_inc.h
@@ -0,0 +1,14 @@
+#if 0
+#include "xbyak_translate/add.h"
+#include "xbyak_translate/cmp.h"
+#include "xbyak_translate/mov.h"
+#include "xbyak_translate/movq.h"
+#include "xbyak_translate/movss.h"
+#include "xbyak_translate/sub.h"
+//#include "xbyak_translate/vmovups.h"
+#include "xbyak_translate/vmulps.h"
+//#include "xbyak_translate/vbroadcastss.h"
+//#include "xbyak_translate/vfmadd132ps.h"
+//#include "xbyak_translate/vfmadd213ps.h"
+//#include "xbyak_translate/vfmadd231ps.h"
+#endif
