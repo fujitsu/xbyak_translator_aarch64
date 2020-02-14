3b034f1eb3c61564ed700ba03a78f778a0fbf842
Kentaro Kawakami <kawakami.k@fujitsu.com>
2020-02-14 14:05:19 +0900

    Bug fix of translateVMOVHLPS

--- translator/include/xbyak_translator_for_aarch64/instructions/vmovhlps.h
+++ translator/include/xbyak_translator_for_aarch64/instructions/vmovhlps.h
@@ -3,11 +3,16 @@ void translateVMOVHLPS(xed_decoded_inst_t *p) {
   struct xt_a64fx_operands_struct_t a64;
   xt_construct_a64fx_operands(p, &a64);
 
-  /* 2020/02/14 11:06 */
+  /* 2020/02/14 14:03 */
 
   /* Col=V103*/
   if (false || (true)) {
-    CodeGeneratorAArch64::ext(xa::VReg16B(a64.dstIdx), xa::VReg16B(a64.src2Idx),
-                              xa::VReg16B(a64.srcIdx), 8);
+    CodeGeneratorAArch64::mov(xa::VReg2D(a64.dstIdx)[1],
+                              xa::VReg2D(a64.srcIdx)[1]);
+  }
+  /* Col=W103*/
+  if (false || (true)) {
+    CodeGeneratorAArch64::mov(xa::VReg2D(a64.dstIdx)[0],
+                              xa::VReg2D(a64.src2Idx)[1]);
   }
 }
