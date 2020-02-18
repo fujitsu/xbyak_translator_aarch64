a59660a8897a7179ea2f5233d6ea8bd804c5d048
KURUME Yuta <kurume.yuta@jp.fujitsu.com>
2020-02-18 10:12:26 +0900

    remove .vandps.h.swp

--- translator/include/xbyak_translator_for_aarch64/instructions/vandps.h
+++ translator/include/xbyak_translator_for_aarch64/instructions/vandps.h
@@ -3,14 +3,14 @@ void translateVANDPS(xed_decoded_inst_t *p) {
   struct xt_a64fx_operands_struct_t a64;
   xt_construct_a64fx_operands(p, &a64);
 
-//if(a64.dstWidth==256 && a64.PredType==A64_PRED_INIT && a64.EVEXb == 0
-//   && a64.dstType==A64_OP_REG && a64.srcType==A64_OP_REG ) {
-	std::cout << "debug" << std::endl;
+if(a64.dstWidth==256 && a64.PredType==A64_PRED_INIT && a64.EVEXb == 0
+   && a64.dstType==A64_OP_REG && a64.srcType==A64_OP_REG ) {
+//	std::cout << "debug" << std::endl;
 	a64.zTmpIdx = xt_push_zreg();
-//	CodeGeneratorAArch64::ldr(xa::ZReg(a64.zTmpIdx), xa::ptr(X_TMP_ADDR));
-//	CodeGeneratorAArch64::and_(xa::ZReg(a64.dstIdx).d,xa::ZReg(a64.srcIdx).d, xa::ZReg(a64.zTmpIdx).d);
-//	xt_pop_zreg();
-//	CodeGeneratorAArch64::mov(xa::ZReg(a64.dstIdx).d, P_MSB_256/xa::T_m, 0);
-//}
+	CodeGeneratorAArch64::ldr(xa::ZReg(a64.zTmpIdx), xa::ptr(X_TMP_ADDR));
+	CodeGeneratorAArch64::and_(xa::ZReg(a64.dstIdx).d,xa::ZReg(a64.srcIdx).d, xa::ZReg(a64.zTmpIdx).d);
+	xt_pop_zreg();
+	CodeGeneratorAArch64::mov(xa::ZReg(a64.dstIdx).d, P_MSB_256/xa::T_m, 0);
+}
 
 }
