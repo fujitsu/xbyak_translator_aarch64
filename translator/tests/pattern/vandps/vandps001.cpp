064aa425f75f7b294813cc7750e4a239e86cc858
Kentaro Kawakami <kawakami.k@fujitsu.com>
2020-02-19 18:35:04 +0900

    Add non-broadcast memory operand for translateVANDPS

--- translator/tests/pattern/vandps/vandps001.cpp
+++ translator/tests/pattern/vandps/vandps001.cpp
@@ -23,6 +23,7 @@ public:
 
     inputZReg[31].ud_dt[0] = ~uint64_t(0);
     inputZReg[31].ud_dt[2] = ~uint64_t(0);
+    inputZReg[31].ud_dt[3] = ~uint64_t(0);
   }
 
   void setCheckRegFlagAll() {
@@ -35,13 +36,21 @@ public:
     addr = reinterpret_cast<size_t>(&(inputZReg[31].ud_dt[0]));
 
     mov(rax, addr);
-//    vandps(Xmm(1), Xmm(0), ptr[rax]);
+    //    vandps(Xmm(1), Xmm(0), ptr[rax]);
     vandps(Ymm(2), Ymm(0), ptr[rax]);
-//    vandps(Zmm(3), Zmm(0), ptr[rax]);
+    //    vandps(Zmm(3), Zmm(0), ptr[rax]);
 
-//    vandps(Xmm(4), Xmm(0), ptr_b[rax]);
-//    vandps(Ymm(5), Ymm(0), ptr_b[rax]);
-//    vandps(Zmm(6), Zmm(0), ptr_b[rax]);
+    //    vandps(Xmm(4), Xmm(0), ptr_b[rax]);
+    //    vandps(Ymm(5), Ymm(0), ptr_b[rax]);
+    //    vandps(Zmm(6), Zmm(0), ptr_b[rax]);
+
+    //    vandps(Xmm(7), Xmm(7), ptr[rax]);
+    vandps(Ymm(8), Ymm(8), ptr[rax]);
+    //    vandps(Zmm(9), Zmm(9), ptr[rax]);
+
+    //    vandps(Xmm(10), Xmm(10), ptr_b[rax]);
+    //    vandps(Ymm(11), Ymm(11), ptr_b[rax]);
+    //    vandps(Zmm(12), Zmm(12), ptr_b[rax]);
 
     mov(rax, 5);
   }
