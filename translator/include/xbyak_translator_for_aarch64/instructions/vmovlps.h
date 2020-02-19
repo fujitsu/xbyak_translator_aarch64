7c1d8b937475920cfb783d2999d90431f137d6a8
sakamoto <sakamoto-takuya@jp.fujitsu.com>
2020-02-19 10:31:59 +0900

    apply excel translation

--- translator/include/xbyak_translator_for_aarch64/instructions/vmovlps.h
+++ translator/include/xbyak_translator_for_aarch64/instructions/vmovlps.h
@@ -3,13 +3,30 @@ void translateVMOVLPS(xed_decoded_inst_t *p) {
   struct xt_a64fx_operands_struct_t a64;
   xt_construct_a64fx_operands(p, &a64);
   xt_dump_a64fx_operands(&a64);
-  if (a64.dstType == A64_OP_REG && a64.srcType == A64_OP_REG && a64.src2Type == A64_OP_MEM) {
+
+  /* 2020/02/19 13:02 */
+  /* Col=U103*/
+  if(false ||(a64.dstType==A64_OP_REG&&a64.srcType==A64_OP_REG&&a64.src2Type==A64_OP_MEM&&true)) {
     a64.vTmpIdx = xt_push_vreg(&a64);
+  }
+  /* Col=Y103*/
+  if(false ||(a64.dstType==A64_OP_REG&&a64.srcType==A64_OP_REG&&a64.src2Type==A64_OP_MEM&&true)) {
     CodeGeneratorAArch64::mov(xa::VReg2D(a64.dstIdx)[1], xa::VReg2D(a64.srcIdx)[1]);
-    CodeGeneratorAArch64::ldr(xa::ZReg(a64.vTmpIdx), xa::ptr(X_TMP_ADDR));
+  }
+  /* Col=AD103*/
+  if(false ||(a64.dstType==A64_OP_REG&&a64.srcType==A64_OP_REG&&a64.src2Type==A64_OP_MEM&&true)) {
+    CodeGeneratorAArch64::ldr(xa::QReg(a64.vTmpIdx), xa::ptr(X_TMP_ADDR));
+  }
+  /* Col=AH103*/
+  if(false ||(a64.dstType==A64_OP_REG&&a64.srcType==A64_OP_REG&&a64.src2Type==A64_OP_MEM&&true)) {
     CodeGeneratorAArch64::mov(xa::VReg2D(a64.dstIdx)[0], xa::VReg2D(a64.vTmpIdx)[0]);
+  }
+  /* Col=AL103*/
+  if(false ||(a64.dstType==A64_OP_REG&&a64.srcType==A64_OP_REG&&a64.src2Type==A64_OP_MEM&&true)) {
     xt_pop_vreg();
-  } else if (a64.dstType == A64_OP_MEM && a64.srcType == A64_OP_REG) {
-    CodeGeneratorAArch64::str(xa::ZReg(a64.srcIdx), xa::ptr(X_TMP_ADDR));
+  }
+  /* Col=AP103*/
+  if(false ||(a64.dstType==A64_OP_MEM&&a64.srcType==A64_OP_REG&&true)) {
+    CodeGeneratorAArch64::st1(xa::VReg2D(a64.srcIdx)[0], xa::ptr(X_TMP_ADDR));
   }
 }
