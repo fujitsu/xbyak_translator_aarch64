d33146f09307ceeef2b9b3a1572e896394d78867
Kentaro Kawakami <kawakami.k@fujitsu.com>
2020-02-21 16:57:56 +0900

    Refactoring vmovd.h

--- translator/include/xbyak_translator_for_aarch64/instructions/vmovd.h
+++ translator/include/xbyak_translator_for_aarch64/instructions/vmovd.h
@@ -5,32 +5,35 @@ void translateVMOVD(xed_decoded_inst_t *p) {
 
   /* 2020/02/21 14:56 */
 
-
-
   /* Col=V103*/
-  if(false ||(a64.dstWidth==128&&a64.PredType==A64_PRED_NO&&a64.dstType==A64_OP_REG&&a64.srcType==A64_OP_REG&&true)||(a64.dstWidth==128&&a64.dstType==A64_OP_REG&&a64.srcType==A64_OP_MEM&&true)) {
+  if (false ||
+      (a64.dstWidth == 128 && a64.PredType == A64_PRED_NO &&
+       a64.dstType == A64_OP_REG && a64.srcType == A64_OP_REG && true) ||
+      (a64.dstWidth == 128 && a64.dstType == A64_OP_REG &&
+       a64.srcType == A64_OP_MEM && true)) {
     CodeGeneratorAArch64::bic(xa::VReg(a64.dstIdx).s4, 0);
   }
 
-
-
   /* Col=Z103*/
-  if(false ||(a64.dstWidth==128&&a64.PredType==A64_PRED_NO&&a64.dstType==A64_OP_REG&&a64.srcType==A64_OP_REG&&true)) {
+  if (false ||
+      (a64.dstWidth == 128 && a64.PredType == A64_PRED_NO &&
+       a64.dstType == A64_OP_REG && a64.srcType == A64_OP_REG && true)) {
     CodeGeneratorAArch64::fmov(xa::SReg(a64.dstIdx), xa::WReg(a64.srcIdx));
   }
   /* Col=AA103*/
-  if(false ||(a64.dstWidth==32&&a64.dstType==A64_OP_REG&&a64.srcType==A64_OP_REG&&true)) {
+  if (false || (a64.dstWidth == 32 && a64.dstType == A64_OP_REG &&
+                a64.srcType == A64_OP_REG && true)) {
     CodeGeneratorAArch64::fmov(xa::WReg(a64.dstIdx), xa::SReg(a64.srcIdx));
   }
 
   /* Col=AC103*/
-  if(false ||(a64.dstWidth==128&&a64.dstType==A64_OP_REG&&a64.srcType==A64_OP_MEM&&true)) {
+  if (false || (a64.dstWidth == 128 && a64.dstType == A64_OP_REG &&
+                a64.srcType == A64_OP_MEM && true)) {
     CodeGeneratorAArch64::ldr(xa::SReg(a64.dstIdx), xa::ptr(X_TMP_ADDR));
   }
   /* Col=AD103*/
-  if(false ||(a64.dstWidth==32&&a64.dstType==A64_OP_MEM&&a64.srcType==A64_OP_REG&&true)) {
+  if (false || (a64.dstWidth == 32 && a64.dstType == A64_OP_MEM &&
+                a64.srcType == A64_OP_REG && true)) {
     CodeGeneratorAArch64::str(xa::SReg(a64.srcIdx), xa::ptr(X_TMP_ADDR));
   }
-  
-  
 }
