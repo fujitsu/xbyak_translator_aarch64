04e97cbb05d69599dd3badf1e376b65ce077fef0
kurihara <kouji3211@fujitsu.com>
2020-03-11 13:32:45 +0900

    create pinsrb and test pattern

---
+++ translator/include/xbyak_translator_for_aarch64/instructions/pinsrb.h
@@ -0,0 +1,57 @@
+void translatePINSRB(xed_decoded_inst_t *p) {
+  namespace xa = Xbyak_aarch64;
+  struct xt_a64fx_operands_structV3_t a64;
+  xt_construct_a64fx_operandsV3(p, &a64);
+
+/* 2020/03/11 13:29 */
+#define CG64 CodeGeneratorAArch64
+  xt_reg_idx_t dstIdx;
+  xt_reg_idx_t srcIdx;
+  xed_uint64_t uimm;
+
+  /* Col=W103*/
+  if (false || (a64.operands[0].opName == XED_OPERAND_REG0 &&
+                a64.operands[1].opName == XED_OPERAND_REG1 && true) ||
+      (a64.operands[0].opName == XED_OPERAND_REG0 &&
+       a64.operands[1].opName == XED_OPERAND_MEM0 && true)) {
+    dstIdx = a64.operands[0].regIdx;
+  }
+  /* Col=X103*/
+  if (false || (a64.operands[0].opName == XED_OPERAND_REG0 &&
+                a64.operands[1].opName == XED_OPERAND_REG1 && true)) {
+    srcIdx = a64.operands[1].regIdx;
+  }
+
+  /* Col=AB103*/
+  if (false || (a64.operands[0].opName == XED_OPERAND_REG0 &&
+                a64.operands[1].opName == XED_OPERAND_REG1 && true) ||
+      (a64.operands[0].opName == XED_OPERAND_REG0 &&
+       a64.operands[1].opName == XED_OPERAND_MEM0 && true)) {
+    uimm = a64.operands[2].uimm;
+  }
+
+  /* Col=AE103*/
+  if (false || (a64.operands[0].opName == XED_OPERAND_REG0 &&
+                a64.operands[1].opName == XED_OPERAND_MEM0 && true)) {
+    CG64::ldr(W_TMP_0, xa::ptr(X_TMP_ADDR));
+  }
+  /* Col=AF103*/
+  if (false || (a64.operands[0].opName == XED_OPERAND_REG0 &&
+                a64.operands[1].opName == XED_OPERAND_MEM0 && true)) {
+    CG64::and_(W_TMP_0, W_TMP_0, 255);
+  }
+  /* Col=AG103*/
+  if (false || (a64.operands[0].opName == XED_OPERAND_REG0 &&
+                a64.operands[1].opName == XED_OPERAND_REG1 && true)) {
+    CG64::and_(W_TMP_0, xa::WReg(srcIdx), 255);
+  }
+  /* Col=AH103*/
+  if (false || (a64.operands[0].opName == XED_OPERAND_REG0 &&
+                a64.operands[1].opName == XED_OPERAND_REG1 && true) ||
+      (a64.operands[0].opName == XED_OPERAND_REG0 &&
+       a64.operands[1].opName == XED_OPERAND_MEM0 && true)) {
+    CG64::mov(xa::VReg(dstIdx).b[uimm], W_TMP_0);
+  }
+
+#undef CG64
+}
