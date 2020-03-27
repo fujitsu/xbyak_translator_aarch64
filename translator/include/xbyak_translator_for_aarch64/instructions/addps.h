e2f2491dc8c8144b548668092b1a69f3262a5661
KURUME Yuta <kurume.yuta@jp.fujitsu.com>
2020-03-27 09:23:48 +0900

    addps OK, Congratulation! with new test pattern

--- translator/include/xbyak_translator_for_aarch64/instructions/addps.h
+++ translator/include/xbyak_translator_for_aarch64/instructions/addps.h
@@ -3,7 +3,7 @@ void translateADDPS(xed_decoded_inst_t *p) {
   struct xt_a64fx_operands_struct_t a64;
   xt_construct_a64fx_operands(p, &a64);
 
-  /* 2020/03/19 14:18 */
+  /* 2020/03/27 09:19 */
   bool isValid = false;
 #define CG64 CodeGeneratorAArch64
 
@@ -32,18 +32,18 @@ void translateADDPS(xed_decoded_inst_t *p) {
     CodeGeneratorAArch64::not_(xa::PRegB(a64.pTmpIdx), xa::PReg(15),
                                xa::PRegB(14));
   }
-  /* Col=AA143*/
+  /* Col=AB143*/
   if (false || (a64.dstWidth == 128 && a64.PredType == A64_PRED_NO &&
                 a64.EVEXb == 0 && a64.srcType == A64_OP_REG && true)) {
-    CodeGeneratorAArch64::add(xa::ZRegD(a64.dstIdx), xa::PReg(a64.pTmpIdx),
-                              xa::ZRegD(a64.srcIdx));
+    CodeGeneratorAArch64::fadd(xa::ZRegS(a64.dstIdx), xa::PReg(a64.pTmpIdx),
+                               xa::ZRegS(a64.srcIdx));
   }
 
   /* Col=AC143*/
   if (false || (a64.dstWidth == 128 && a64.PredType == A64_PRED_NO &&
                 a64.EVEXb == 0 && a64.srcType == A64_OP_MEM && true)) {
-    CodeGeneratorAArch64::add(xa::ZRegD(a64.dstIdx), xa::PReg(a64.pTmpIdx),
-                              xa::ZRegD(a64.zTmpIdx));
+    CodeGeneratorAArch64::fadd(xa::ZRegS(a64.dstIdx), xa::PReg(a64.pTmpIdx),
+                               xa::ZRegS(a64.zTmpIdx));
   }
 
   /* Col=AW143*/
