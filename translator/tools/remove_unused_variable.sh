cab51d8fb22b8f6acece19aca2991be8a04eabba
Kentaro Kawakami <kawakami.k@fujitsu.com>
2020-04-21 21:38:10 +0900

    Add script to remove unused variables

---
+++ translator/tools/remove_unused_variable.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -u
+unset tmpfile
+
+LIST_KEYWORDS="uimm dstIdx srcIdx src2Idx src3Idx maskIdx zTmpIdx zTmp2Idx zTmp3Idx zTmp4Idx pTmpIdx pTmp2Idx"
+FILE_IN=${1}
+
+atexit() {
+    [[ -n ${tmpfile-} ]] && rm -f "$tmpfile"
+}
+
+
+trap atexit EXIT
+trap 'rc=$?; trap - EXIT; atexit; exit $?' INT PIPE TERM
+
+
+echo "Remove unused varialbe:${FILE_IN}"
+
+
+tmpfile=$(mktemp "/tmp/${0##*/}.tmp.XXXXXX")
+
+for i in ${LIST_KEYWORDS} ; do
+    num=`grep -w ${i} ${FILE_IN} | wc -l`
+    if [ ${num} -le 1 ] ; then
+	grep -v ${i} ${FILE_IN} > ${tmpfile}
+	mv ${tmpfile} ${FILE_IN}
+    fi
+done
+
+
