388c88c34d1411932a2f0945fdd454c881463aeb
Kentaro Kawakami <kawakami.k@fujitsu.com>
2020-06-18 00:21:35 +0900

    refactoring

--- translator/include/xbyak_translator_for_aarch64/instructions/vmovmskps.h
+++ translator/include/xbyak_translator_for_aarch64/instructions/vmovmskps.h
@@ -20,10 +20,10 @@ void translateVMOVMSKPS(xed_decoded_inst_t *p) {
   struct xt_a64fx_operands_structV3_t a64;
   xt_construct_a64fx_operandsV3(p, &a64);
   bool isValid = false;
-  xt_reg_idx_t dstIdx;
-  xt_reg_idx_t srcIdx;
-  xt_reg_idx_t zTmpIdx;
-  xt_reg_idx_t pTmpIdx;
+  xt_reg_idx_t dstIdx = XT_REG_INVALID;
+  xt_reg_idx_t srcIdx = XT_REG_INVALID;
+  xt_reg_idx_t zTmpIdx = XT_REG_INVALID;
+  xt_reg_idx_t pTmpIdx = XT_REG_INVALID;
 
   /* Col=Y119*/
   if (false || (a64.operands[1].regClass == XED_REG_CLASS_XMM && true) ||
