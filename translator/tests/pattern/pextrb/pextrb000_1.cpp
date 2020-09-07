b1469c6029012f5c45cb932b621c7c1f32bb4369
Kentaro Kawakami <kawakami.k@fujitsu.com>
2020-09-07 23:26:49 +0900

    pextrb: Add "pextrb reg, xmm, imm8", "pextrb m8, xmm, imm8" and test patterns

---
+++ translator/tests/pattern/pextrb/pextrb000_1.cpp
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright 2020 FUJITSU LIMITED
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+#include "test_generator2.h"
+
+class TestPtnGenerator : public TestGenerator {
+public:
+  void setInitialRegValue() {
+    /* Here modify arrays of inputGenReg, inputPredReg, inputZReg */
+    setInputZregAllRandomHex();
+  }
+
+  void setCheckRegFlagAll() {
+    /* Here modify arrays of checkGenRegMode, checkPredRegMode, checkZRegMode */
+  }
+
+  void genJitTestCode() {
+    /* Here write JIT code with x86_64 mnemonic function to be tested. */
+
+    /* Register index is within SSE range. */
+    pextrb(r8, Xmm(0), 0);
+    pextrb(r9, Xmm(1), 1);
+    pextrb(r10, Xmm(2), 2);
+    pextrb(r11, Xmm(3), 4);
+    pextrb(r12, Xmm(4), 8);
+    pextrb(r13, Xmm(5), 15);
+    pextrb(r14, Xmm(6), 6);
+    pextrb(r15, Xmm(7), 7);
+  }
+};
+
+int main(int argc, char *argv[]) {
+  /* Initializing arrays of inputData, inputGenReg, inputPredReg, inputZReg,
+   * checkGenRegMode, checkPredRegMode,checkZRegMode */
+  TestPtnGenerator gen;
+
+  /* Set bool output_jit_on_, bool exec_jit_on_ = 0; */
+  gen.parseArgs(argc, argv);
+
+  /* Generate JIT code and get function pointer */
+  void (*f)();
+  if (gen.isOutputJitOn()) {
+    f = (void (*)())gen.gen();
+  }
+
+  /* Dump generated JIT code to a binary file */
+  gen.dumpJitCode();
+
+  /* 1:Execute JIT code, 2:dump all register values, 3:dump register values to
+   * be checked */
+  if (gen.isExecJitOn()) {
+    /* Before executing JIT code, dump inputData, inputGenReg, inputPredReg,
+     * inputZReg. */
+    gen.dumpInputReg();
+    f();                 /* Execute JIT code */
+    gen.dumpOutputReg(); /* Dump all register values */
+    gen.dumpCheckReg();  /* Dump register values to be checked */
+  }
+
+  return 0;
+}
