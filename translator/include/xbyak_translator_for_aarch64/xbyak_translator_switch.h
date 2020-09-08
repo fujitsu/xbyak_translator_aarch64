ac05cf2d0657cef129ea74a50e0d68bc4b03ebc6
Kentaro Kawakami <kawakami.k@fujitsu.com>
2020-09-08 14:17:01 +0900

    vgatherdps: invalidate, because it is still buggy

--- translator/include/xbyak_translator_for_aarch64/xbyak_translator_switch.h
+++ translator/include/xbyak_translator_for_aarch64/xbyak_translator_switch.h
@@ -2199,7 +2199,7 @@ case XED_ICLASS_VFRCZSS:
 case XED_ICLASS_VGATHERDPD:
   break;
 case XED_ICLASS_VGATHERDPS:
-  translateVGATHERDPS(&xedd);
+  //  translateVGATHERDPS(&xedd);
   break;
 case XED_ICLASS_VGATHERPF0DPD:
   break;
